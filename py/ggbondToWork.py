ea52e164f6c3706c62f5425df77204fbeb928d68
justinssjia <justinssjia@tencent.com>
2023-05-19 17:14:40 +0800

    init

---
+++ py/ggbondToWork.py
@@ -0,0 +1,37 @@
+import datetime
+import json
+import os
+import sys
+
+import git
+from chinese_calendar import is_workday
+
+date = datetime.datetime.now().date()
+path = sys.path[0]  # 当前文件目录
+path = os.path.dirname(path)  # 工程目录
+ggBondProject = git.Repo.init(path)  # 初始化git仓库
+
+if is_workday(date):
+    print("%s是工作日" % date)
+    remote = ggBondProject.remote()
+    remote.pull()
+    print("代码拉取成功")
+    jsonFile = open(path + "/holidaysAtGGBond.json", 'r')
+    jsonJ = json.load(jsonFile)
+    print(jsonJ)
+    jsonFile.close()
+    if jsonJ["GG"]:
+        result = os.popen(path + "/sh/ggbondTest.sh")
+        print(result.read())
+    else:
+        print("请假啦~~~~~")
+else:
+    print("%s是休息日粗去玩~~~" % date)
+
+remote = ggBondProject.remote()
+ggBondProject.index.add(".")
+ggBondProject.index.commit(date.__str__())
+repose = remote.push()
+print(repose)
+print("日志更新")
+print("\n")
---
+++ sh/ggbondTest.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "上班打卡"
+
+sleep 9
+sleep1
+
+echo "上班打卡end"
