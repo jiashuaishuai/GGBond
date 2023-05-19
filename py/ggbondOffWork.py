4446897c1cb16c396d8ef6c0a7d821a2bad8b36a
justinssjia <justinssjia@tencent.com>
2023-05-19 17:39:49 +0800

    2023-05-19

---
+++ py/ggbondOffWork.py
@@ -0,0 +1,23 @@
+
+import datetime
+import json
+import os
+import sys
+
+from chinese_calendar import is_workday
+path = sys.path[0]  # 当前文件目录
+path = os.path.dirname(path)  # 工程目录
+date = datetime.datetime.now().date()
+if is_workday(date):
+    print("%s是工作日" % date)
+    jsonFile = open(path + "/holidaysAtGGBond.json", 'r')
+    jsonJ = json.load(jsonFile)
+    print(jsonJ)
+    jsonFile.close()
+    if jsonJ["GG"]:
+        result = os.popen(path + "/sh/ggbondTest2.sh")
+        print(result.read())
+    else:
+        print("请假啦~~~~~")
+else:
+    print("%s是休息日粗去玩~~~" % date)
---
+++ sh/ggbondTest2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "下班"
+
+sleep 9
+sleep1
+
+echo "下班end"
