73b7330388f1ecccdca5ea17e1f9f707e55fb620
justinssjia <justinssjia@tencent.com>
2023-07-19 09:29:45 +0800

    设备型号

--- sh/offWork.sh
+++ sh/offWork.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "执行下班任务中~~~"
-/Users/jss/Library/Android/sdk/platform-tools/adb shell input keyevent 224
-/Users/jss/Library/Android/sdk/platform-tools/adb shell input swipe 1358 1458 1358 300 
-/Users/jss/Library/Android/sdk/platform-tools/adb shell input text 777777
-/Users/jss/Library/Android/sdk/platform-tools/adb shell am force-stop com.tencent.wework
-/Users/jss/Library/Android/sdk/platform-tools/adb shell am start com.tencent.wework/com.tencent.wework.launch.LaunchSplashActivity
+/Users/jss/Library/Android/sdk/platform-tools/adb -s WRU6R20428002766 shell input keyevent 224
+/Users/jss/Library/Android/sdk/platform-tools/adb -s WRU6R20428002766 shell input swipe 1358 1458 1358 300
+/Users/jss/Library/Android/sdk/platform-tools/adb -s WRU6R20428002766 shell input text 777777
+/Users/jss/Library/Android/sdk/platform-tools/adb -s WRU6R20428002766 shell am force-stop com.tencent.wework
+/Users/jss/Library/Android/sdk/platform-tools/adb -s WRU6R20428002766 shell am start com.tencent.wework/com.tencent.wework.launch.LaunchSplashActivity
 sleep 9
 #/Users/jss/Library/Android/sdk/platform-tools/adb shell input tap 1532 1532
 #sleep 1
@@ -12,6 +12,6 @@ sleep 9
 #sleep 9
 #/Users/jss/Library/Android/sdk/platform-tools/adb shell input tap 1930 1138
 #sleep 3
-/Users/jss/Library/Android/sdk/platform-tools/adb shell input keyevent 26
+/Users/jss/Library/Android/sdk/platform-tools/adb -s WRU6R20428002766 shell input keyevent 26
 echo "执行下班任务成功！！！！"
 
--- sh/toWork.sh
+++ sh/toWork.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "执行上班任务中~~~~"
-/Users/jss/Library/Android/sdk/platform-tools/adb shell input keyevent 224
-/Users/jss/Library/Android/sdk/platform-tools/adb shell input swipe 1358 1458 1358 300 
-/Users/jss/Library/Android/sdk/platform-tools/adb shell input text 777777
-/Users/jss/Library/Android/sdk/platform-tools/adb shell am force-stop com.tencent.wework
-/Users/jss/Library/Android/sdk/platform-tools/adb shell am start com.tencent.wework/com.tencent.wework.launch.LaunchSplashActivity
+/Users/jss/Library/Android/sdk/platform-tools/adb -s WRU6R20428002766 shell input keyevent 224
+/Users/jss/Library/Android/sdk/platform-tools/adb -s WRU6R20428002766 shell input swipe 1358 1458 1358 300
+/Users/jss/Library/Android/sdk/platform-tools/adb -s WRU6R20428002766 shell input text 777777
+/Users/jss/Library/Android/sdk/platform-tools/adb -s WRU6R20428002766 shell am force-stop com.tencent.wework
+/Users/jss/Library/Android/sdk/platform-tools/adb -s WRU6R20428002766 shell am start com.tencent.wework/com.tencent.wework.launch.LaunchSplashActivity
 sleep 9
-/Users/jss/Library/Android/sdk/platform-tools/adb shell input keyevent 26
+/Users/jss/Library/Android/sdk/platform-tools/adb -s WRU6R20428002766 shell input keyevent 26
 echo "执行上班任务成功！！！！"
